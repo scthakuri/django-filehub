8d4655932a290984cc9fb057d1236de110b9ef54
Suresh Chand <scthakuri12a@gmail.com>
2024-12-01 18:06:31 +0545

    update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="django_filehub",
-    version="0.1.1",
+    version="0.1.4",
+    author="Suresh Chand",
+    author_email="scthakuri12a@gmail.com",
     packages=find_packages(),
     install_requires=[
-        "Django>=3.2",
+        "django",
+        "requests"
     ],
-    classifiers=[
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
         "Framework :: Django",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -21,4 +29,8 @@ setup(
     },
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/scthakuri/django-filehub/issues",
+        "Source": "https://github.com/scthakuri/django-filehub",
+    },
 )
