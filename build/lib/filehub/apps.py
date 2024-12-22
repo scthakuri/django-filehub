4526394ea95b754ffa11886a775295f6c16239a8
Suresh Chand <scthakuri12a@gmail.com>
2024-12-23 00:33:32 +0545

    clean project

--- build/lib/filehub/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FilehubConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "filehub"
-
-    def ready(self):
-        import filehub.signals
